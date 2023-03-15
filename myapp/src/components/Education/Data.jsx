a1afae7bc4b888143ec47a9f469171d6afe923fe
Rachit Saxena <72144798+horcruxxxx@users.noreply.github.com>
2023-03-15 23:00:00 +0530

    Education Component
    Completed

---
+++ myapp/src/components/Education/Data.jsx
@@ -0,0 +1,52 @@
+const Data = [
+    {
+      id: 1,
+      category: "Graduation",
+      icon: "icon-briefcase",
+      year: "2019 - present",
+      title: "Bachelor's Degree",
+      desc: "Hindustan College of Science and Technology",
+    },
+    {
+      id: 2,
+      category: "Graduation",
+      icon: "icon-badge",
+      year: "CGPA - 7.8",
+      title: "Specialization",
+      desc: "Computer Science Engineering",
+    },
+    {
+      id: 3,
+      category: "Intermediate",
+      icon: "icon-graduation",
+      year: "2018 - 2019",
+      title: "Intermediate",
+      desc: "Agra Public School",
+    },
+    {
+      id: 4,
+      category: "Intermediate",
+      icon: "icon-badge",
+      year: " Percenntage - 75.4%",
+      title: "Specialization ",
+      desc: "Science",
+    },
+    {
+      id: 5,
+      category: "Highschool",
+      icon: "icon-graduation",
+      year: "2016 - 2017",
+      title: "High School",
+      desc: "Agra Public School",
+    },
+    {
+      id: 6,
+      category: "Highschool",
+      icon: "icon-badge",
+      year: "CGPA - 85%",
+      title: "Specialization ",
+      desc: "Science",
+    },
+  ];
+
+  export default Data;
