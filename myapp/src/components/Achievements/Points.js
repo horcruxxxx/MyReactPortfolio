8af358d4ff8e7bb7dcb4c1f0bcc1efa742e692f7
Rachit Saxena <72144798+horcruxxxx@users.noreply.github.com>
2023-03-18 07:30:07 +0530

    Achievements Component
    Completed.
    used "npm i Swiper" for Pagination

---
+++ myapp/src/components/Achievements/Points.js
@@ -0,0 +1,64 @@
+import React from "react";
+import Image1 from "../../assets/avatar-1.svg";
+import Image2 from "../../assets/avatar-3.svg";
+
+const Points = [
+    {
+      id: 1,
+      image: Image1,
+      title: "John Doe",
+      subtitle: "Product designer at Dribble",
+      comment:
+        "I enjoy working with the theme and learn so much. You       guys make the process fun and interesting. Good luck! 👍",
+    },
+    {
+      id: 2,
+      image: Image2,
+      title: "John Doe",
+      subtitle: "Product designer at Dribble",
+      comment:
+        "I enjoy working with the theme and learn so much. You       guys make the process fun and interesting. Good luck! 👍",
+    },
+    {
+      id: 3,
+      image: Image2,
+      title: "John Doe",
+      subtitle: "Product designer at Dribble",
+      comment:
+        "I enjoy working with the theme and learn so much. You       guys make the process fun and interesting. Good luck! 👍",
+    },
+    {
+      id: 4,
+      image: Image1,
+      title: "John Doe",
+      subtitle: "Product designer at Dribble",
+      comment:
+        "I enjoy working with the theme and learn so much. You       guys make the process fun and interesting. Good luck! 👍",
+    },
+    {
+      id: 5,
+      image: Image1,
+      title: "John Doe",
+      subtitle: "Product designer at Dribble",
+      comment:
+        "I enjoy working with the theme and learn so much. You       guys make the process fun and interesting. Good luck! 👍",
+    },
+    {
+      id: 6,
+      image: Image2,
+      title: "John Doe",
+      subtitle: "Product designer at Dribble",
+      comment:
+        "I enjoy working with the theme and learn so much. You       guys make the process fun and interesting. Good luck! 👍",
+    },
+    {
+      id: 7,
+      image: Image1,
+      title: "John Doe",
+      subtitle: "Product designer at Dribble",
+      comment:
+        "I enjoy working with the theme and learn so much. You       guys make the process fun and interesting. Good luck! 👍",
+    },
+  ];
+
+  export default Points;
